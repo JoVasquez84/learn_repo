1262f69b174523357fd3381ab0634fcd6ad049e9
JoVasquez <joanna7vasquez@gmail.com>
2021-08-13 13:20:34 -0700

    Edited test suites

--- app.js
+++ app.js
@@ -1,172 +1,135 @@
 //setup read from .env
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const result = dotenv.config();
 if (result.error) {
   throw result.error;
 }
 const { parsed: envs } = result;
 
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const { json } = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const app = express();
-const knex = require('knex')(require('./knexfile.js')[envs.NODE_ENV]);
+const knex = require("knex")(require("./knexfile.js")[envs.NODE_ENV]);
 
 app.use(express.json());
 
-app.get('/movies/:id', function(req,res) {
-    knex
-        .select('*')
-        .from('movies')
-        .where({
-            id:req.params.id}
-            )
-        .then((data) => {
-            if (data.length === 0) {
-                res.status(404).json ({
-                    message:
-                    'Movie ID not found'
-                })
-            }
-            return data
-        }
-        )
-        .then(data => res.status(200).json(data))
-        .catch((err) => {
-            if (res.status(400)) { 
-                res.status(400).json({
-                    message:
-                    'Invalid ID supplied'
-                })
-            }
-          })
-})
+app.get("/movies/:id", function (req, res) {
+  knex
+    .select("*")
+    .from("movies")
+    .where({
+      id: req.params.id,
+    })
+    .then((data) => {
+      if (data.length === 0) {
+        res.status(404).json({
+          message: "Movie ID not found",
+        });
+      }
+      return data;
+    })
+    .then((data) => res.status(200).json(data))
+    .catch((err) => {
+      if (res.status(400)) {
+        res.status(400).json({
+          message: "Invalid ID supplied",
+        });
+      }
+    });
+});
 
-app.get('/movies',function(req,res) {
-    //if there was a query parameter passed then handel it
-    if (req.query.title) {
-        //get query string
-        let inputTitle = req.query.title
-        //let regex = /[a-zA-Z]/g; // /^[a-zA-Z\s]*$/
-        //let regex = /^[a-z0-9A-Z\s]*$/; // ^[\w\s]+$
-        let regex = /[\w\s]+$/; // ^[\w\s]+$
-        let matches = inputTitle.match(regex)
-        console.log('the regex results', matches)
-        if (!matches) {
-            console.log('invalid search')
-            res.status(400).json({
-                message:
-                'Invalid titleQuery supplied'
-            })
-        } else {        
-        knex
-        .select('*')
-        .from('movies')
-        .where('title', 'ILIKE', `%${inputTitle}%`)
+app.get("/movies", function (req, res) {
+  //if there was a query parameter passed then handel it
+  if (req.query.title) {
+    //get query string
+    let inputTitle = req.query.title;
+    let regex = /[\w\s]+$/; // ^[\w\s]+$
+    let matches = inputTitle.match(regex);
+    console.log("the regex results", matches);
+    if (!matches) {
+      console.log("invalid search");
+      res.status(400).json({
+        message: "Invalid titleQuery supplied",
+      });
+    } else {
+      knex
+        .select("*")
+        .from("movies")
+        .where("title", "ILIKE", `%${inputTitle}%`)
         //if the search query is empty
         .then((data) => {
-            if (data.length === 0) {
-                res.status(404).json ({
-                    message:
-                    'Your query string returned no results'
-                })
-            } 
-            return data
-            }           
-        )
-        .then ((data) => res.status(200).json(data))
+          if (data.length === 0) {
+            res.status(404).json({
+              message: "Your query string returned no results",
+            });
+          }
+          return data;
+        })
+        .then((data) => res.status(200).json(data))
         //if the query was input was invalid
-        .catch(err =>
-            res.status(400).json({
-                message:
-                'Invalid titleQuery supplied'
-            })
-         );
-        }
-        
-    } else {
+        .catch((err) =>
+          res.status(400).json({
+            message: "Invalid titleQuery supplied",
+          })
+        );
+    }
+  } else {
     //if not then do the below query because there was no query param
     //this is the knex query
     knex
-        .select('*')
-        .from('movies')
-        .then(data => res.status(200).json(data))
-        .catch(err =>
-            res.status(404).json({
-                message:
-                'The data you are looking for could not be found. Please try again'
-            })
-         );
-        }
-    //knex query ends here
+      .select("*")
+      .from("movies")
+      .then((data) => res.status(200).json(data))
+      .catch((err) =>
+        res.status(404).json({
+          message: "The data you are looking for could not be found. Please try again",
+        })
+      );
+  }
+  //knex query ends here
 });
 
-app.post('/movies', function(req,res){
-    let result;
-    const newMovie = req.body;
-    //sconsole.log(newMovie)
-    if (newMovie.title && newMovie.runtime && newMovie.release_year && newMovie.director) {
-        knex('movies')
-        .insert([
-            {title:newMovie.title, 
-             runtime:newMovie.runtime, 
-             release_year:newMovie.release_year, 
-             director:newMovie.director}]
-        )
-        .returning('*')
-        .then( data =>
-            res.status(200).json(data)
-        )
-        .catch(err =>
-            res.status(400).json({
-                message:
-                'Invalid Input From insert'
-            })
-         );
-    } else {
+app.post("/movies", function (req, res) {
+  let result;
+  const newMovie = req.body;
+  if (newMovie.title && newMovie.runtime && newMovie.release_year && newMovie.director) {
+    knex("movies")
+      .insert([
+        { title: newMovie.title, runtime: newMovie.runtime, release_year: newMovie.release_year, director: newMovie.director },
+      ])
+      .returning("*")
+      .then((data) => res.status(200).json(data))
+      .catch((err) =>
         res.status(400).json({
-            message:
-            'Invalid Input'
+          message: "Invalid Input From insert",
         })
-    }
-})
+      );
+  } else {
+    res.status(400).json({
+      message: "Invalid Input",
+    });
+  }
+});
 
-app.delete('/movies/:id', function (req,res){
-    const toDelete= req.params.id;
-    if (toDelete) {
-        knex('movies')
-        .where('id', toDelete)
-        .del()
-        .returning('*')
-        .then( data => res.status(200).json(data))
-        .catch(err =>
-            res.status(400).json({
-                message:
-                'Invalid Delete Call'
-            })
-         );
-    } else {
+app.delete("/movies/:id", function (req, res) {
+  const toDelete = req.params.id;
+  if (toDelete) {
+    knex("movies")
+      .where("id", toDelete)
+      .del()
+      .returning("*")
+      .then((data) => res.status(200).json(data))
+      .catch((err) =>
         res.status(400).json({
-            message:
-            'Invalid Input'
+          message: "Invalid Delete Call",
         })
-    }
-})
-
-
-
-
-// knex('users').where({
-//     first_name: 'Test',
-//     last_name:  'User'
-//   }).select('id')
-//   Outputs:
-//   select `id` from `users` where `first_name` = 'Test' and `last_name` = 'User'
-
-// term = '%' + term + '%';
-
-// .orWhere( knex.raw( 'question  LIKE ?', [ term ] ) )
-// .orWhere( knex.raw( 'note      LIKE ?', [ term ] ) )
-// .orWhere( knex.raw( 'user_name LIKE ?', [ term ] ) )
+      );
+  } else {
+    res.status(400).json({
+      message: "Invalid Input",
+    });
+  }
+});
 
 module.exports = app;
--- test/movies.test.js
+++ test/movies.test.js
@@ -1,38 +1,84 @@
 const app = require('../app');
-const request = require('supertest');
+const supertest = require('supertest')
+const request = supertest(app);
 
-async function fetchData(endpoint) {
-  return request(app)
-  .get(endpoint)
-  .set('Accept','application/json')
-}
-
-describe('the GET /movies path', () => {
-  it('returns all movies', async () => {
-    const response = await fetchData('/movies')
-    expect(response.status).toEqual(200)
-    expect(response.body).toHaveLength(13)
+describe("Test the root path", () => {
+  test("It should response the GET method", async () => {
+    const response = await request.get("/movies");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(13);
   });
 });
 
-describe('the GET /movies:id path', () => {
-  it('returns the movies by Id', async () => {
-    const response = await fetchData('/movies/1')
-    expect(response.body).toHaveLength(1)
-  });
 
-  it('invalid id if input is invalue', async () => {
-    const response = await fetchData('/movies/{')
-    expect(response.status).toEqual(400)
-    expect(response.body.message).toEqual('Invalid ID supplied')
+describe('POST /api/v1/session/api_key', () => {
+  it('should create api key', async () => {
+  await request
+  .post('/movies')
+  .send({
+    "title": "From Paris With Returning",
+    "runtime": 94,
+    "release_year": 2010,
+    "director": "Pierre Morel"
+    })
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .then((res) => {
+    console.log(res)
+  expect(res.text).to.have.property('id');
   });
+  });
+  }); 
 
-  // it('returns ID not found if it is not there', async () => {
-  //   const response2 = await fetchData('/movies/200')
-  //   expect(response2.status).toEqual(404)
-  //   expect(response2.body.message).toEqual('Movie ID not found')
+// async function fetchData(endpoint) {
+//   return request
+//   .get(endpoint)
+//   .set('Accept','application/json')
+// }
+
+// describe('the GET /movies path', () => {
+//   it('returns all movies', async () => {
+//     const response = await fetchData('/movies')
+//     expect(response.status).toEqual(200)
+//     expect(response.body).toHaveLength(13)
+//   });
+// });
+
+// describe('the GET /movies path', () => {
+//   it('gets the test endpoint', async done => {
+//     const response = await request.get('/movies')  
+//     expect(response.status).toBe(200)
+//     done()
+//   })
+
+
+  // it('returns all movies', async () => {
+  //   const response = await fetchData('/movies')
+  //   expect(response.status).toEqual(200)
+  //   expect(response.body).toHaveLength(13)
   // });
-});
+//});
+
+
+// describe('the GET /movies:id path', () => {
+//   it('returns the movies by Id', async () => {
+//     const response = await request.get('/movies/1')
+//     expect(response.status).toBe(200)
+//     expect(response.body).toHaveLength(1)
+//   });
+
+//   it('invalid id if input is invalue', async () => {
+//     const response = await fetchData('/movies/{')
+//     expect(response.status).toEqual(400)
+//     expect(response.body.message).toEqual('Invalid ID supplied')
+//   });
+
+//   it('returns ID not found if it is not there', async () => {
+//     const response2 = await fetchData('/movies/200')
+//     expect(response2.status).toEqual(404)
+//     expect(response2.body.message).toEqual('Movie ID not found')
+//   });
+// });
 
 
 afterAll(() => console.log('1 - afterAll'));
